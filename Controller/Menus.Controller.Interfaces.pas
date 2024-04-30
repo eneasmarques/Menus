4a7ca673eab0e2fc31dc2eac95e096c0a0ea039c
Enéas Marques <eneasmarques@outlook.com.br>
2024-04-30 07:17:34 -0300

    feat: Cria Factorys ListBox e ListBoxItens

--- Controller/Menus.Controller.Interfaces.pas
+++ Controller/Menus.Controller.Interfaces.pas
@@ -2,6 +2,16 @@ unit Menus.Controller.Interfaces;
 
 interface
 
+type
+  iControllerListBoxItensFactory = interface
+    ['{4F1DC6D8-6E94-4240-A45F-348BD30A15D1}']
+  end;
+
+  iControllerListBoxFactory = interface
+    ['{9995F794-8669-42C5-A37E-7536A6E8EAD5}']
+  end;
+
 implementation
 
 end.
+
