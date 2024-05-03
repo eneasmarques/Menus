bef4438309e745dc3ccbce046dc3a5f12fc39c7f
Enéas Marques <eneasmarques@outlook.com.br>
2024-05-03 06:59:35 -0300

    feat: Cria procedure Exibir para adicionar ListBox ao Layout

--- Controller/Menus.Controller.Interfaces.pas
+++ Controller/Menus.Controller.Interfaces.pas
@@ -6,43 +6,38 @@ uses
   System.Classes, FMX.Types;
 
 type
-  iControllerListBoxDefault = interface;
   iControllerListBoxItensDefault = interface;
+  iControllerListBoxDefault = interface;
 
   iControllerListBoxItensFactory = interface
     ['{4F1DC6D8-6E94-4240-A45F-348BD30A15D1}']
-    function Default : iControllerListBoxItensDefault;
+    function Default: iControllerListBoxItensDefault;
   end;
 
   iControllerListBoxFactory = interface
     ['{9995F794-8669-42C5-A37E-7536A6E8EAD5}']
-    function Default(Container : TComponent) : iControllerListBoxDefault;
+    function Default(Container: TComponent): iControllerListBoxDefault;
   end;
 
   iControllerListBoxItensDefault = interface
     ['{D0F53CA1-75DA-41C7-9B6B-2112EFC92F12}']
-    function Name(Value : String) : iControllerListBoxItensDefault;
-    function Text(Value : String) : iControllerListBoxItensDefault;
-    function StyleLookup(Value : String) : iControllerListBoxItensDefault;
-    function OnClick(Value : TNotifyEvent) :  iControllerListBoxItensDefault;
-    function Item : TFMXObject;
+    function Name(Value: String): iControllerListBoxItensDefault;
+    function Text(Value: String): iControllerListBoxItensDefault;
+    function StyleLookup(Value: String): iControllerListBoxItensDefault;
+    function OnClick(Value: TNotifyEvent): iControllerListBoxItensDefault;
+    function Item: TFMXObject;
   end;
 
   iControllerListBoxDefault = interface
     ['{878A6FB1-8528-403C-A624-E0C83FB7686C}']
-    function Name(Value : String) : iControllerListBoxDefault;
-    function Align(Value : TAlignLayout) : iControllerListBoxDefault;
-    function Height(Value : Integer) : iControllerListBoxDefault;
-    function AddItem(Value : TFmxObject) : iControllerListBoxDefault;
-    function Lista : TFmxObject;
-  end;
-
-  iControllerListBoxItensForm = interface
-    ['{036FA257-B924-484C-8CB9-5BD4619EDCFD}']
-    function Show : TFmxObject;
+    function Name(Value: String): iControllerListBoxDefault;
+    function Align(Value: TAlignLayout): iControllerListBoxDefault;
+    function Height(Value: Integer): iControllerListBoxDefault;
+    function AddItem(Value: TFMXObject): iControllerListBoxDefault;
+    function Lista: TFMXObject;
+    procedure Exibir;
   end;
 
 implementation
 
 end.
-
--- Controller/Menus.Controller.ListBox.Default.pas
+++ Controller/Menus.Controller.ListBox.Default.pas
@@ -6,34 +6,39 @@ uses
   Menus.Controller.Interfaces, FMX.ListBox, System.Classes, FMX.Types;
 
 type
-  TControllerListBoxDefault = class(TInterfacedObject, iControllerListBoxDefault)
-    private
-      FContainer : TComponent;
-      FListBox : TListBox;
-    public
-      constructor Create(Container : TComponent);
-      destructor Destroy; override;
-      class function New(Container : TComponent) : iControllerListBoxDefault;
-      function Lista : TFmxObject;
-      function AddItem(Value : TFmxObject) : iControllerListBoxDefault;
-      function Name(Value : String) : iControllerListBoxDefault;
-      function Align(Value : TAlignLayout) : iControllerListBoxDefault;
-      function Height(Value : Integer) : iControllerListBoxDefault;
+  TControllerListBoxDefault = class(TInterfacedObject,
+    iControllerListBoxDefault)
+  private
+    FContainer: TComponent;
+    FListBox: TListBox;
+  public
+    constructor Create(Container: TComponent);
+    destructor Destroy; override;
+    class function New(Container: TComponent): iControllerListBoxDefault;
+    function Lista: TFmxObject;
+    function AddItem(Value: TFmxObject): iControllerListBoxDefault;
+    function Name(Value: String): iControllerListBoxDefault;
+    function Align(Value: TAlignLayout): iControllerListBoxDefault;
+    function Height(Value: Integer): iControllerListBoxDefault;
+    procedure Exibir;
   end;
 
 implementation
 
+uses
+  FMX.Layouts;
+
 { TControllerListBoxDefault }
 
-function TControllerListBoxDefault.AddItem(
-  Value: TFmxObject): iControllerListBoxDefault;
+function TControllerListBoxDefault.AddItem(Value: TFmxObject)
+  : iControllerListBoxDefault;
 begin
   Result := Self;
   FListBox.AddObject(Value);
 end;
 
-function TControllerListBoxDefault.Align(
-  Value: TAlignLayout): iControllerListBoxDefault;
+function TControllerListBoxDefault.Align(Value: TAlignLayout)
+  : iControllerListBoxDefault;
 begin
   Result := Self;
   FListBox.Align := Value;
@@ -54,28 +59,32 @@ begin
   inherited;
 end;
 
-function TControllerListBoxDefault.Height(
-  Value: Integer): iControllerListBoxDefault;
+procedure TControllerListBoxDefault.Exibir;
+begin
+  TLayout(FContainer).AddObject(FListBox);
+end;
+
+function TControllerListBoxDefault.Height(Value: Integer)
+  : iControllerListBoxDefault;
 begin
   Result := Self;
   FListBox.Height := Value;
 end;
-
 
 function TControllerListBoxDefault.Lista: TFmxObject;
 begin
   Result := FListBox;
 end;
 
-function TControllerListBoxDefault.Name(
-  Value: String): iControllerListBoxDefault;
+function TControllerListBoxDefault.Name(Value: String)
+  : iControllerListBoxDefault;
 begin
   Result := Self;
   FListBox.Name := Value;
 end;
 
-class function TControllerListBoxDefault.New(
-  Container: TComponent): iControllerListBoxDefault;
+class function TControllerListBoxDefault.New(Container: TComponent)
+  : iControllerListBoxDefault;
 begin
   Result := Self.Create(Container);
 end;
